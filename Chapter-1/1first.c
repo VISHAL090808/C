a79e203b3c4058fd09ee2d773dbaefc6c2a2d88a
Vishal <vishaldwivedi717@gmail.com>
2023-02-11 15:15:41 +0530

    Chapter 1

---
+++ Chapter-1/1first.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+/*
+This is our frist c program
+which is awesome
+*/
+
+int main (){
+    int tom;
+    int Tom;
+    int error_code;
+    // 'J' --> a character 
+    printf("hello world");
+    return 0;
+}
+ 
---
+++ Chapter-1/2vaiables.c
@@ -0,0 +1,23 @@
+# include<stdio.h>
+/*
+This is our frist c program
+which is awesome
+*/
+
+int main ()
+{  
+    int a = 4;
+    // int b = 8.5; // not recommended because 8.5 is not an integer
+
+    float b = 8.5;
+    char c = 'u';
+    int d = 45;
+    int e = 45 + 4; 
+    printf("The value of a is %d \n", a);
+    printf("The value of a is %f \n", b);
+    printf("The value of a is %c \n", c);
+    printf("Sum of a and d is %d \n",  a + d);
+    printf("Sum of a and d is %d \n", e);
+    return 0;
+}
+ 
